b5844a4d032bc9c88117e71342f33cbba0129379
Daniel Hocking <dhocking@unh.edu>
2015-08-03 16:49:45 -0400

    converted to use all years of data rather than just the most recent year now that I knew it would work and we can also add a temporal component explicitly

--- Code/O-U_GMRF_Prepare_Data.R
+++ Code/O-U_GMRF_Prepare_Data.R
@@ -30,17 +30,17 @@ df_bkt_reduced <- df_bkt %>%
                    width = mean(width))
 
 # just use the most recent date a site was sampled to avoid temporal trends at sites
-recent_site_visits <- df_bkt_reduced %>%
-  dplyr::ungroup() %>%
-  dplyr::group_by(location_id) %>%
-  dplyr::summarise(date = max(date)) %>%
-  dplyr::mutate(visit = paste0(location_id, "_", date))
-
-df_visit <- df_bkt_reduced %>%
-  dplyr::select(visit, location_id, date, year, month, length_sample, width)
-
-df_recent_visits <- left_join(recent_site_visits, df_visit) %>%
-  distinct() # i don't understand why this was needed but only way not duplicated -- maybe do to grouping of one or both of the dataframes - groupings seems to cause problems, maybe should always ungroup at end of pipes.
+# recent_site_visits <- df_bkt_reduced %>%
+#   dplyr::ungroup() %>%
+#   dplyr::group_by(location_id) %>%
+#   dplyr::summarise(date = max(date)) %>%
+#   dplyr::mutate(visit = paste0(location_id, "_", date))
+# 
+ df_visit <- df_bkt_reduced %>%
+   dplyr::select(visit, location_id, date, year, month, length_sample, width)
+# 
+# df_recent_visits <- left_join(recent_site_visits, df_visit) %>%
+#   distinct() # i don't understand why this was needed but only way not duplicated -- maybe due to grouping of one or both of the dataframes - groupings seems to cause problems, maybe should always ungroup at end of pipes.
 
 # expand to 3-pass for all visits
 foo <- df_bkt_reduced %>%
@@ -60,7 +60,10 @@ df_bkt_3_pass <- df_bkt_3_pass %>%
   tidyr::spread(key = pass, value = count) 
 
 # reduce to most recent site-visit
-df_trout <- left_join(df_recent_visits, df_bkt_3_pass)
+#df_trout <- left_join(df_recent_visits, df_bkt_3_pass)
+
+# combine covariate data back in
+df_trout <- left_join(df_bkt_3_pass, df_visit) %>% distinct()
 
 df <- left_join(family, df_trout, by = c("child_name" = "location_id")) %>%
   dplyr::select(child_name, parent_b, dist_b, child_b, NodeLat, NodeLon, pass_1, pass_2, pass_3, date, length_sample, width)
@@ -85,8 +88,10 @@ year <- as.factor(df$year)
 dummies <- model.matrix(~year)
 length_std <- (df$length_sample - mean(df$length_sample)) / sd(df$length_sample)
 width_std <- (df$width - mean(df$width)) / sd(df$width)
-X_ij = cbind(dummies[ , 2:ncol(dummies)], length_std, width_std)
+X_ij = cbind(dummies, length_std, width_std) #[ , 2:ncol(dummies)]
 
 df_stds <- data.frame(parameter = c("length", "width"), means = c(mean(df$length_sample), mean(df$width)), sds = c(sd(df$length_sample), sd(df$width)))
 
-save(c_ip, X_ij, df_stds, df, file = "Data/Prepared_Data.RData")
+t_i <- df$year
+
+save(c_ip, X_ij, df_stds, t_i, df, family, file = "Data/Prepared_Data.RData")
--- Code/O-U_GMRF_White_River.R
+++ Code/O-U_GMRF_White_River.R
@@ -12,7 +12,12 @@ library(dplyr)
 #######################
 load("Data/Prepared_Data.RData")
 
-family <- df
+# df = dataframe with all data including sites with multiple passes (multiple instances of each child)
+# family = dataframe with unique child rows. Other columns are parents of each child, lat, lon, and other data associated with each child node.
+# C_ip matrix of counts at site-year i on electrofish survey pass p
+# X_ij matrix of covariates (j) for each site-year i as a design matrix including the intercept (column of 1s)
+# t_i vector of length i indicating the survey year for each site-year visit
+# df_stds dataframe with three columns of the parameter name, means, stds used for z-score standardization of the continuously-distributed independent variables in X_ij
 
 #######################
 # Fit in TMB
@@ -29,7 +34,7 @@ Version = "OU_GMRF_v1d"
 ExtraDetectionSD = TRUE
 
 # Compile
-if(FALSE){
+if(FALSE) {
   dyn.unload(dynlib(paste0("Code/", Version)))
   file.remove( paste0("Code/", Version,c(".o",".dll")) )
 }
@@ -60,9 +65,9 @@ if(Version=="OU_GMRF_v1b") {
   c_i <- as.vector(dplyr::bind_rows(foo, dplyr::select(N_cs, No))$No)
 } # end if version b statement
 
-if(Version=="OU_GMRF_v1a") Data = list( "n_i"=length(c_i), "n_b"=nrow(family), "c_i"=c_i, "d_i"=family[,'child_b']-1, "parent_b"=family[,'parent_b']-1, "child_b"=family[,'child_b']-1, "dist_b"=family[,'dist_b'])
-if(Version=="OU_GMRF_v1b") Data = list( "n_i"=length(c_i), "n_b"=nrow(family), "c_i"=c_i, "d_i"=family[,'child_b']-1, "X_ij"=X_ij, "parent_b"=family[,'parent_b']-1, "child_b"=family[,'child_b']-1, "dist_b"=family[,'dist_b'])
-if(Version%in%c("OU_GMRF_v1c","OU_GMRF_v1d")) Data = list( "n_i"=dim(c_ip)[1], "n_b"=nrow(family), "c_ip"=as.matrix(c_ip), "d_i"=family[,'child_b']-1, "X_ij"=X_ij, "parent_b"=family[,'parent_b']-1, "child_b"=family[,'child_b']-1, "dist_b"=family[,'dist_b'])
+if(Version=="OU_GMRF_v1a") Data = list( "n_i"=length(c_i), "n_b"=nrow(family), "c_i"=c_i, "d_i"=family[,'child_b']-1, "parent_b"=family[,'parent_b']-1, "child_b"=child_b-1, "dist_b"=family[,'dist_b'])
+if(Version=="OU_GMRF_v1b") Data = list( "n_i"=length(c_i), "n_b"=nrow(family), "c_i"=c_i, "d_i"=family[,'child_b']-1, "X_ij"=X_ij, "parent_b"=family[,'parent_b']-1, "child_b"=child_b-1, "dist_b"=family[,'dist_b'])
+if(Version%in%c("OU_GMRF_v1c","OU_GMRF_v1d")) Data = list( "n_i"=dim(c_ip)[1], "n_b"=nrow(family), "c_ip"=as.matrix(c_ip), "d_i"=df[,'child_b']-1, "X_ij"=X_ij, "parent_b"=family[ ,'parent_b']-1, "child_b"=family[ ,'child_b']-1, "dist_b"=family[,'dist_b'])
 
 if(Version=="OU_GMRF_v1a") Params = list( "log_theta"=log(1), "log_SD"=log(1), "log_mean"=log(1), "Epsiloninput_d"=rnorm(Data$n_b))
 if(Version=="OU_GMRF_v1b") Params = list( "log_theta"=log(1), "log_SD"=log(1), "log_mean"=log(1), "gamma_j"=rep(0,ncol(Data$X_ij)), "Epsiloninput_d"=rnorm(Data$n_b))
@@ -99,7 +104,7 @@ Sdreport = sdreport( obj )
 SD = sdreport( obj, bias.correct=TRUE )
 SD$unbiased$value
 
-n = nrow(family)
+n = nrow(df)
 data.frame(v1 = SD$unbiased$value[1:n], v2 = SD$unbiased$value[(n+1):(2*n)], v3 = SD$unbiased$value[(2*n+1):(3*n)])
 
 N_unbias <- SD$unbiased$value[1:n]
@@ -122,16 +127,27 @@ abline( a=0, b=1, lty="dotted")
 # get outpts for maping rho and N
 N_i <- Report$lambda_ip[ , 1]
 
-df_N <- data.frame(child_name = family$child_name, c_sum = rowSums(family[ , c("pass_1", "pass_2", "pass_3")]), N_i, N_unbias, N_sd, rho_b = Report$rho_b, p = Report$detectprob_ip, pass_1 = family$pass_1, pass_2 = family$pass_2, pass_3 = family$pass_3)
+df_N <- data.frame(child_name = df$child_name, c_sum = rowSums(df[ , c("pass_1", "pass_2", "pass_3")]), N_i, N_unbias, N_sd, p = Report$detectprob_ip, pass_1 = df$pass_1, pass_2 = df$pass_2, pass_3 = df$pass_3)
+
+# only 1 rho per node - add child_name and join back to df_N if rho doesn't change by year
+rho_b = data.frame(child_name = family$child_name, rho_b = Report$rho_b)
+df_N <- left_join(df_N, rho_b)
 
 # check if predictions of N are at least as large as the number of individuals caught (assume: complete closure during removal passes and no mis-identification)
 df_N <- df_N %>%
   dplyr::mutate(p_miss_total = (1-p.1)^3,
-                problem = ifelse(c_sum > N_i, TRUE, FALSE),
+               # problem = ifelse(c_sum > N_i, TRUE, FALSE),
                 N_100 = N_i / df$length_sample * 100)
 df_N
 
 df_N %>%
-  dplyr::select(c_sum, N_i, N_unbias, N_sd, problem, pass_1, pass_2, pass_3) %>%
+  dplyr::select(c_sum, N_i, N_unbias, N_sd, pass_1, pass_2, pass_3, p_miss_total) %>%
   dplyr::filter(!is.na(c_sum))
+
+# save output for Kyle to map
+df_kyle <- df_N %>%
+  dplyr::select(child_name, N_100, rho_b) # if rho doesn't change filter to unique child
+
+write.csv(df_kyle, file = "Output/N_correlation.csv")
+
 
